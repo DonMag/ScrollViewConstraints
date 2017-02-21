5e67a9bcb0665675b9bc78c98059bc13ac122829
DonMag <ACP@ACPs-iMac.local>
2017-02-21 15:50:30 -0500

    first look

---
+++ ScrollViewConstraints/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ScrollViewConstraints
+//
+//  Created by Don Mag on 2/21/17.
+//  Copyright © 2017 DonMag. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ScrollViewConstraints/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ScrollViewConstraints
+//
+//  Created by Don Mag on 2/21/17.
+//  Copyright © 2017 DonMag. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
